eeaee63d68531e0fc0bcce844d1a45561f268b0c
Rolando Mejia <rolandoaguilarmejia@gmail.com>
2021-02-07 15:58:03 -0600

    Programa terminado

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TB1 = new System.Windows.Forms.TextBox();
+            this.suma = new System.Windows.Forms.Button();
+            this.TB2 = new System.Windows.Forms.TextBox();
+            this.total = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.TB3 = new System.Windows.Forms.TextBox();
+            this.TB4 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(104, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Numero de turismos";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(104, 108);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(121, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Numero de todoterrenos";
+            // 
+            // TB1
+            // 
+            this.TB1.Location = new System.Drawing.Point(302, 68);
+            this.TB1.Name = "TB1";
+            this.TB1.Size = new System.Drawing.Size(100, 20);
+            this.TB1.TabIndex = 2;
+            this.TB1.TextChanged += new System.EventHandler(this.TB1_TextChanged);
+            // 
+            // suma
+            // 
+            this.suma.Location = new System.Drawing.Point(164, 256);
+            this.suma.Name = "suma";
+            this.suma.Size = new System.Drawing.Size(75, 23);
+            this.suma.TabIndex = 3;
+            this.suma.Text = "Aceptar";
+            this.suma.UseVisualStyleBackColor = true;
+            this.suma.Click += new System.EventHandler(this.suma_Click);
+            // 
+            // TB2
+            // 
+            this.TB2.Location = new System.Drawing.Point(302, 101);
+            this.TB2.Name = "TB2";
+            this.TB2.Size = new System.Drawing.Size(100, 20);
+            this.TB2.TabIndex = 4;
+            // 
+            // total
+            // 
+            this.total.AutoSize = true;
+            this.total.Location = new System.Drawing.Point(210, 282);
+            this.total.Name = "total";
+            this.total.Size = new System.Drawing.Size(13, 13);
+            this.total.TabIndex = 8;
+            this.total.Text = "  ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(104, 148);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(114, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Capacidad de turismos";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(104, 187);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(135, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Capacidad de todoterrenos";
+            // 
+            // TB3
+            // 
+            this.TB3.Location = new System.Drawing.Point(302, 148);
+            this.TB3.Name = "TB3";
+            this.TB3.Size = new System.Drawing.Size(100, 20);
+            this.TB3.TabIndex = 11;
+            // 
+            // TB4
+            // 
+            this.TB4.Location = new System.Drawing.Point(302, 187);
+            this.TB4.Name = "TB4";
+            this.TB4.Size = new System.Drawing.Size(100, 20);
+            this.TB4.TabIndex = 12;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(66, 29);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(159, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Por favor introduzca estos datos";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(514, 356);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.TB4);
+            this.Controls.Add(this.TB3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.total);
+            this.Controls.Add(this.TB2);
+            this.Controls.Add(this.suma);
+            this.Controls.Add(this.TB1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Calculo de necesidades combustible";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TB1;
+        private System.Windows.Forms.Button suma;
+        private System.Windows.Forms.TextBox TB2;
+        private System.Windows.Forms.Label total;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox TB3;
+        private System.Windows.Forms.TextBox TB4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        double Num1, Num2, Num3, Num4;
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TB1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void suma_Click(object sender, EventArgs e)
+        {
+            double Suma;
+            Num1 = Convert.ToDouble(TB1.Text);
+            Num2 = Convert.ToDouble(TB2.Text);
+            Num3 = Convert.ToDouble(TB3.Text);
+            Num4 = Convert.ToDouble(TB4.Text);
+            Suma = Num1 * Num3 + Num2 * Num4;
+            MessageBox.Show("Las necesidades totales de combustible son de " +Suma +" litros");
+        }
+    }
+}
